dc9d90c6e81dd635a9a644acf01f68ce0840f6a7
Aman Mittal <amanmittal3305@gmail.com>
2025-04-27 09:04:25 +0530

    Project Completed

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,9 +5,9 @@ import App from './App.jsx'
 import 'remixicon/fonts/remixicon.css'
 
 createRoot(document.getElementById('root')).render(
-  
+  <StrictMode>
     <App />
-  
+  </StrictMode>
 )
 
 // 4:54:16
